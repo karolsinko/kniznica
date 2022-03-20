de462c0efd5a6e5331a58ff483fc06ba046437bd
JakubU7 <5d2uhrincat@gmail.com>
2022-03-20 20:40:12 +0100

    Úprava UserController+UserService

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,67 +7,38 @@ import java.util.List;
 
 @RestController
 public class UserController {
+
     private List<User> users;
 
-    public UserController(){
-        this.users = init();
+    private UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
     }
 
-    private List<User> init(){
-        List<User> users = new ArrayList<>();
-        User user1 = new User();
-        user1.setId(1L);
-        user1.setFirstName("Jožko");
-        user1.setLastName("Mrkvička");
-        user1.setEmail("jozko@abc.sk");
-        users.add(user1);
-
-        User user2 = new User();
-        user2.setId(2L);
-        user2.setFirstName("Hanka");
-        user2.setLastName("Nová");
-        user2.setEmail("hanka@abc.sk");
-        users.add(user2);
-        return users;
+    @GetMapping("/api/users") //filtered users with user last name
+    public List<User> getUsers(@RequestParam(required = false) String userlastName){
+        return userService.getUsers(userlastName);
     }
 
-    @GetMapping("/api/users")
-    public List<User> getUsers(@RequestParam(required = false) String User){
-        if (User == null){
-            return this.users;
-        }
-        List<User> filteredUsers = new ArrayList<>();
-
-        for(User user : users){
-            if (user.getUser().equals(User)){
-                filteredUsers.add(user);
-            }
-        }
-        return filteredUsers;
-
-    }
-    @GetMapping("/api/users/{userId}")
-    public User getUser(@PathVariable Integer userId){
-        return this.users.get(userId);
-
-    }
-
-
-    @PostMapping("/api/users")
-    public Integer createUser(@RequestBody User user){
-        this.users.add(user);
-
-        return this.users.size() -1;
-    }
     @DeleteMapping("/api/users/{userId}")
-    public void deleteUser(@PathVariable Integer customerId){
-        this.users.remove(this.users.get(customerId));
-
+    public void deleteUser(@PathVariable Integer userId){
+        userService.deleteUser(userId);
     }
+
     @PutMapping("/api/users/{userId}")
-    public void updateUser(@PathVariable Integer userId, @RequestBody User user){
-        this.users.get(userId).setUser(user.getUser());
+    public List<User> putUser(@PathVariable Integer userId, @RequestBody User user){
+        return userService.putUser(userId, user);
     }
+
+    @GetMapping("/api/userid") //filtered users with user last name
+    public List<User> getUsersId(@RequestParam(required = false) String userId){
+        return userService.getUsersId(userId);
+    }
+
+    @PostMapping("/api/users") //creating new user
+    public List<User> createUser(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
 }
-
-
